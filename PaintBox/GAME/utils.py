add7a002a61248d4821d27740feeeecb1d6dd2d1
Ben alaluf <94129183+benalaluf@users.noreply.github.com>
2023-09-21 16:01:21 +0300

    init

--- PaintBox/GAME/utils.py
+++
@@ -1,6 +0,0 @@
-def get_color(color_number: int, all_colors):
-    for i in range(len(all_colors)):
-        if color_number == i:
-            return all_colors[i]
-
-    return None
---
+++ main.py
@@ -0,0 +1,3 @@
+import PyQt5
+
+print("Game Is Game")
